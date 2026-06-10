28a4c5560e10b67bcceaa36bf99e50f53395a27c
jenishghadiya <jenishvghadiya@gmail.com>
2026-06-10 22:01:55 +0530

    fix: update API base URLs for demo and test environments

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -5,16 +5,18 @@ let fwgBaseUrl = "http://localhost:82";
 let razorpayMerchantId = "rzp_test_sb4SYqLcmeDGqi";
 let environment = "development";
 
-if (hostname === "fgiit.com" || hostname === "www.fgiit.com") {
-  baseUrl = "https://api.fgiit.com";
-  fwgBaseUrl = 'https://app-api.fgiit.com';
+if (hostname === "demo-session.fggroup.in" || hostname === "www.demo-session.fggroup.in") {
+  baseUrl = "https://api.fggroup.in";
+  fwgBaseUrl = 'https://app-api.fggroup.in';
   environment = 'production'
   razorpayMerchantId = "rzp_live_WxBhDlHqqY0CYX";
 } else if (hostname === "test.fgiit.com") {
   baseUrl = "https://dev-api.fgiit.com";
   fwgBaseUrl = 'https://fg-app-dev-api.fgiit.com';
 } else {
-  baseUrl = "http://localhost:82";
+  //  baseUrl = "https://dev-api.fggroup.in";
+  baseUrl = 'http://localhost:80';
+  // fwgBaseUrl = 'https://fg-app-dev-api.fggroup.in';
   fwgBaseUrl = 'http://localhost:82';
 }
 
